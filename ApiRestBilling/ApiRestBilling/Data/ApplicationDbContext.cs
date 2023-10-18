14541d74f007a098a78cb219282051c6a6d69b2c
Vladimir Cortes A <vccortesa@eafit.edu.co>
2023-10-18 15:13:58 -0500

    feat(conf): add connection string and packages entity framework

---
+++ ApiRestBilling/ApiRestBilling/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ApiRestBilling.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            
+        }
+    }
+}
